49110b1bf7b292602d15b4c4da885c0ca724bf4d
codeRockStar1992 <rlopez2219@outlook.com>
2022-05-22 20:08:28 -0500

    Refactor: Initial commit for Ggame

---
+++ components/layout.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+interface LayoutProps {
+  children: JSX.Element
+  title?: string
+  replaceTitle?: boolean
+}
+
+const defaultTitle = 'GGame'
+
+function Layout({ children, title, replaceTitle = false }: LayoutProps) {
+  return (
+    <>
+      <Head>
+        <title>
+          {title
+            ? replaceTitle
+              ? title
+              : `${defaultTitle} | ${title}`
+            : defaultTitle}
+        </title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content='This is the GGame description' />
+      </Head>
+      <div>{children}</div>
+    </>
+  )
+}
+
+export default Layout
+export { defaultTitle }
