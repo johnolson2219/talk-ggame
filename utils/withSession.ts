75e5d200282d8749854495bb4acd71a621d502f5
codeRockStar1992 <rlopez2219@outlook.com>
2022-06-10 01:08:45 -0500

    Feat: Create utility for iron-session

---
+++ utils/withSession.ts
@@ -0,0 +1,40 @@
+import { withIronSessionApiRoute, withIronSessionSsr } from 'iron-session/next'
+import type {
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  NextApiHandler,
+} from 'next'
+
+const IRON_SESSION_PASSWORD = process.env.IRON_SESSION_PASSWORD as string
+
+const sessionOptions = {
+  password: IRON_SESSION_PASSWORD,
+  cookieName: 'user',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+}
+
+export function withSessionRoute(handler: NextApiHandler) {
+  return withIronSessionApiRoute(handler, sessionOptions)
+}
+
+export function withSessionSsr<
+  P extends { [key: string]: unknown } = { [key: string]: unknown }
+>(
+  handler: (
+    context: GetServerSidePropsContext
+  ) => GetServerSidePropsResult<P> | Promise<GetServerSidePropsResult<P>>
+) {
+  return withIronSessionSsr(handler, sessionOptions)
+}
+
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: {
+      id: number
+      username: string
+      rol?: 'admin' | 'user'
+    }
+  }
+}
