631bed2b1f669d6e9e87fc5ee49999d6270002e6
codeRockStar1992 <rlopez2219@outlook.com>
2022-06-10 01:12:25 -0500

    Feat: Create user page with todos

---
+++ pages/[user]/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import type { GetServerSideProps, NextPage } from 'next'
+
+import { serveCookies } from '../../utils/serveCookies'
+
+// TODO: Connect user session with iron-session and pass the props
+const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
+  return { props: { ...serveCookies(req) } }
+}
+
+// TODO: Create a interface for own profile and view profile
+const UserProfile: NextPage = ({}) => {
+  return <div>UserProfile</div>
+}
+
+export default UserProfile
+export { getServerSideProps }
