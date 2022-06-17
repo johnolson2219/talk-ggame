79e172d6701d7de48d17cff4aedc2afbfa601c02
codeRockStar1992 <rlopez2219@outlook.com>
2022-06-17 01:15:06 -0500

    Fix: Add restriction to not create user that already exists

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -1,6 +1,6 @@
 import type { NextApiResponse, NextApiRequest } from 'next'
 import bcrypt from 'bcrypt'
-import { createUser } from '../../../services/database'
+import { createUser, getUser } from '../../../services/database'
 import { manageMethod } from '../../../utils/manageMethod'
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -16,12 +16,20 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (badRequest || !sendResponse || !sendInternalError) return
 
   try {
-    const allowedRoles = ['admin', 'user']
-    if (!allowedRoles.includes(rol))
+    const isAllowedRole = !['admin', 'user'].includes(rol)
+    if (isAllowedRole)
       return sendResponse({
         status: 400,
         error: true,
         message: 'Wrong rol. Allowed are admin or user.',
+      })
+
+    const isRepeatedUser = Boolean(await getUser({ username }))
+    if (isRepeatedUser)
+      return sendResponse({
+        status: 400,
+        error: true,
+        message: 'User already exist.',
       })
 
     const hashedPassword = await bcrypt.hash(password, 10)
