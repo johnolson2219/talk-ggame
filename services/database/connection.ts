c3028d33cc827ae4df2b81a2c40085b420fc1cf9
codeRockStar1992 <rlopez2219@outlook.com>
2022-06-17 01:39:27 -0500

    Refactor: Wrap sequelize initialization in a function

--- services/database/connection.ts
+++ services/database/connection.ts
@@ -1,16 +1,24 @@
 import { Sequelize } from 'sequelize'
 
-const HOST = process.env.HOST as string
-const PORT = process.env.PORT as string
-const USER_DB = process.env.USER_DB as string
-const PASSWORD = process.env.PASSWORD as string
-const DATABASE = process.env.DATABASE as string
+let sequelize: Sequelize
 
-const sequelize = new Sequelize(
-  `mysql://${USER_DB}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`,
-  {
-    define: { freezeTableName: true },
-  }
-)
+function connect() {
+  if (sequelize instanceof Sequelize) return
+
+  const HOST = process.env.HOST as string
+  const PORT = process.env.PORT as string
+  const USER_DB = process.env.USER_DB as string
+  const PASSWORD = process.env.PASSWORD as string
+  const DATABASE = process.env.DATABASE as string
+
+  sequelize = new Sequelize(
+    `mysql://${USER_DB}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`,
+    {
+      define: { freezeTableName: true },
+    }
+  )
+}
+
+connect()
 
 export { sequelize }
